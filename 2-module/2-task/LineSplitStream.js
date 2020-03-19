a991155f20692614c4e71bbf9fe81971376e8b5c
Nikita <nikita.postnikov@gazprombank.ru>
2020-03-19 10:19:52 +0300

    solution 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -1,15 +1,20 @@
-const stream = require('stream');
-const os = require('os');
+const stream = require("stream");
+const os = require("os");
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._tab = "";
   }
 
   _transform(chunk, encoding, callback) {
+    this._tab += chunk.toString("utf-8");
+    callback();
   }
 
   _flush(callback) {
+    this._tab.split(os.EOL).map(item => this.push(item));
+    callback();
   }
 }
 
