2b9313bd72dbf7abd73ac0cbe2286eb9241c97a2
Nikita <nikita.postnikov@gazprombank.ru>
2020-03-19 10:22:56 +0300

    solution 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,13 +1,20 @@
-const stream = require('stream');
-const LimitExceededError = require('./LimitExceededError');
+const stream = require("stream");
+
+const LimitExceededError = require("./LimitExceededError");
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._count = 0;
+    this._limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
-
+    const str = chunk.toString("utf-8");
+    this._count += str.length;
+    this._count <= this._limit
+      ? callback(null, str)
+      : callback(new LimitExceededError());
   }
 }
 
