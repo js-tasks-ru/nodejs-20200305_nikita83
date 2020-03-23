0ed7060d27ef91c34bdecd55979e17c390108095
Nikita <nikita.postnikov@gazprombank.ru>
2020-03-23 15:07:35 +0300

    solution 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,22 +1,51 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+
+const http = require("http");
+
+const path = require("path");
+
+const fs = require("fs");
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const urlLength = pathname.split("/").length;
+  console.log(urlLength);
+
+  if (urlLength > 1) {
+    res.statusCode = 400;
+    res.end("No correct request");
+  }
+
+  const filepath = path.join(__dirname, "files", pathname);
 
   switch (req.method) {
-    case 'GET':
+    case "GET":
+      fs.stat(filepath, err => {
+        if (err) {
+          res.statusCode = 404;
+
+          res.end("File not folder");
+        } else {
+          const stream = fs.createReadStream(filepath);
+
+          stream.pipe(res);
+
+          stream.on("end", () => {
+            res.statusCode = 200;
+            res.end();
+          });
+        }
+      });
 
       break;
 
     default:
       res.statusCode = 501;
-      res.end('Not implemented');
+
+      res.end("501");
   }
 });
 
