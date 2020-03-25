7b334f8a0ae08ec0fe885a17d1c174b07d332b57
Nikita <nikita.postnikov@gazprombank.ru>
2020-03-25 14:41:18 +0300

    solution

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,22 +1,81 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const LimitSizeStream = require("./LimitSizeStream");
 
-const server = new http.Server();
-
-server.on('request', (req, res) => {
+const server = http.createServer((req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, "files", pathname);
+
+  if (pathname.split("/").length > 1) {
+    res.statusCode = 400;
+    res.end("No subfolder");
+    return;
+  }
 
   switch (req.method) {
-    case 'POST':
+    case "GET":
+      fs.stat(filepath, err => {
+        if (err) {
+          res.statusCode = 404;
+          res.end("File not folder");
+        }
+        const fileStream = fs.createReadStream(filepath, "utf8");
+        fileStream.on("data", chunk => {
+          res.end(chunk);
+        });
+        fileStream.on("error", () => {
+          fileStream.destroy();
+        });
+      });
+
+    case "POST":
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end();
+        return;
+      }
+      const reqData = (req, callback) => {
+        let body = [];
+        req.on("data", chunk => {
+          body.push(chunk);
+        });
+        req.on("end", () => {
+          callback((body = Buffer.concat(body)));
+        });
+      };
+
+      reqData(req, data => {
+        const limitSizeStream = new LimitSizeStream({ limit: 10000 });
+        const write = fs.createWriteStream(filepath);
+        limitSizeStream.pipe(write);
+
+        limitSizeStream.on("error", () => {
+          if (fs.existsSync(filepath)) {
+            fs.unlinkSync(filepath);
+          }
+          res.statusCode = 413;
+          res.end();
+        });
+
+        limitSizeStream.on("finish", () => {
+          res.statusCode = 201;
+          res.end();
+        });
+
+        if (data.length > 0 && data.length !== undefined) {
+          limitSizeStream.write(data);
+          limitSizeStream.end();
+        }
+      });
 
       break;
 
     default:
       res.statusCode = 501;
-      res.end('Not implemented');
+      res.end("Not implemented");
   }
 });
 
