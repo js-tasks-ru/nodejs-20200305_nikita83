094b5816fc7da5e5a3a88b232bbb3c5fe26630f2
Nikita <nikita.postnikov@gazprombank.ru>
2020-04-05 21:20:54 +0300

    Solution 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,33 @@
+const Product = require('../models/Product');
+
+const mapper = ({
+  id,
+  title,
+  images,
+  category,
+  subcategory,
+  price,
+  description,
+}) => {
+  return {
+    id,
+    title,
+    images,
+    category,
+    subcategory,
+    price,
+    description,
+  };
+};
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const { query } = ctx.query;
+
+  if (!query) {
+    return (ctx.body = { products: [] });
+  }
+
+  const products = await Product.find({ $text: { $search: query } });
+
+  ctx.body = { products: products.map(mapper) };
 };
