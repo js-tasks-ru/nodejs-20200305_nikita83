6f7f8a6fcacfaee80666e97d7e85f63a06fbf48e
Nikita <nikita.postnikov@gazprombank.ru>
2020-04-05 16:06:47 +0300

    solution 6-module 2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,35 @@
-module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+const transform = require('../utils/index');
+
+module.exports.productsBySubcategory = async function productsBySubcategory(
+  ctx,
+  next,
+) {
+  const { subcategory } = ctx.request.query;
+
+  if (!subcategory) return next();
+
+  const products = await Product.find({ subcategory });
+
+  ctx.body = { products: transform(products, 'products') };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+
+  ctx.body = { products: transform(products, 'products') };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const { id } = ctx.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return ctx.throw(400, 'Invalid product id');
+
+  const product = await Product.findById(id);
+
+  if (!product) return ctx.throw(404);
+
+  ctx.body = { product: transform(product, 'product') };
+};
---
+++ 6-module/2-task/utils/index.js
@@ -0,0 +1,67 @@
+module.exports = function transform(data, key) {
+  switch (key) {
+    case 'subcategories':
+      return [].concat(data).map(({ _id, title }) => {
+        return {
+          id: _id,
+          title,
+        };
+      });
+
+    case 'categories':
+      return [].concat(data).map(({ _id, title, subcategories }) => {
+        return {
+          id: _id,
+          title,
+          subcategories: transformResponse(subcategories, 'subcategories'),
+        };
+      });
+
+    case 'products':
+      return [].concat(data).map((item) => {
+        const {
+          _id: id,
+          title,
+          images,
+          category,
+          subcategory,
+          price,
+          description,
+        } = item;
+
+        return {
+          id,
+          title,
+          images,
+          category,
+          subcategory,
+          price,
+          description,
+        };
+      });
+
+    case 'product':
+      const {
+        _id: id,
+        title,
+        images,
+        category,
+        subcategory,
+        price,
+        description,
+      } = data;
+
+      return {
+        id,
+        title,
+        images,
+        category,
+        subcategory,
+        price,
+        description,
+      };
+
+    default:
+      return [];
+  }
+};
