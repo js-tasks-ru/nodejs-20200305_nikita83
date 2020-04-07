70e5d71d8023190989f6feb08a293ec176ab4891
Nikita <nikita.postnikov@gazprombank.ru>
2020-04-07 12:54:39 +0300

    add local strategy

--- 7-module/3-task/libs/strategies/authenticate.js
+++ 7-module/3-task/libs/strategies/authenticate.js
@@ -1,3 +1,13 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (email === undefined) return done(null, false, 'Не указан email');
+  try {
+    const user = await User.findOne({ email });
+    if (user === null) {
+      await User.create({ email: email, displayName: displayName });
+      const persen = await User.findOne({ email });
+      done(false, persen);
+    } else if (typeof user === 'object') return done(false, user);   
+  } catch (err) {
+    done(err);
+  }
 };
